4f62f022855a2698e50dca53bdf8f567fd4a959e
Bojan <bojanj6m@gmail.com>
2017-05-19 06:17:46 -0400

    Using EJS tempate language

--- server.js
+++ server.js
@@ -1,27 +1,21 @@
 import config from './config';
-import apiRouter from './api'
-//import fs from 'fs';
-import express from 'express';
+import apiRouter from './api';
 
+
+import express from 'express';
 const server = express();
 
+server.set('view engine', 'ejs');
+
 server.get('/', (req, res) => {
-	res.send("Hello Express");
+	//res.send("Hello Express");
+	res.render('index', {
+		content: "Hello EJS and <em>Express</em>"
+	});
 });
 
 server.use('/api', apiRouter);
 server.use(express.static('public'));
-
-
-// server.get('/about.html', (req, res) => {
-// 	res.send("about page");
-// });
-
-// server.get('/about.html', (req, res) => {
-// 	fs.readFile('./about.html', (err, data) => {
-// 		res.send(data.toString());
-// 	});
-// });
 
 server.listen(config.port, () => {
 	console.info('Express listening on port', config.port);
@@ -30,5 +24,4 @@ server.listen(config.port, () => {
 
 // npm start
 // curl http://localhost:8080/
-
-// curl http://localhost:8080/about.html
+// npm i -S ejs
