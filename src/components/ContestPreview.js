55d389d46af297268b59eef83d655510938c4a96
Bojan <bojanj6m@gmail.com>
2017-05-19 17:47:01 -0400

    Using Sass with Node

--- src/components/ContestPreview.js
+++ src/components/ContestPreview.js
@@ -2,13 +2,17 @@ import React from 'react';
 
 const ContestPreview = (contest) => (
 	<div className="ContestPreview">
-		<div>
+		<div className="category-name">
 			{contest.categoryName}
 		</div>
-		<div>
+		<div className="contest-name">
 			{contest.contestName}
 		</div>
 	</div>
 )
 
 export default ContestPreview;
+
+
+// https://www.npmjs.com/package/node-sass-middleware
+// npm i -S node-sass-middleware
